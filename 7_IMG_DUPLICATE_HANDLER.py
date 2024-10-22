3c7f654c4cf48e2501d7dd9d5a7baf70bcb78f5c
Vadym Chibrikov <98614057+vchibrikov@users.noreply.github.com>
2024-10-22 16:03:49 +0200

    Add files via upload

---
+++ 7_IMG_DUPLICATE_HANDLER.py
@@ -0,0 +1,28 @@
+import os
+from PIL import Image
+import imagehash
+
+def get_image_hash(image_path):
+    """Compute the hash of an image."""
+    with Image.open(image_path) as img:
+        return imagehash.average_hash(img)
+
+def find_and_delete_duplicates(directory):
+    """Find and delete duplicate images in the specified directory."""
+    seen_hashes = set()
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path):
+            try:
+                img_hash = get_image_hash(file_path)
+                if img_hash in seen_hashes:
+                    print(f"Deleting duplicate image:{file_path}")
+                    os.remove(file_path)
+                else:
+                    seen_hashes.add(img_hash)
+            except Exception as e:
+                print(f"Error processing {file_path}: {e}")
+
+if __name__ == "__main__":
+    directory_path = "/Users/vadymchibrikov/Desktop/FIBREAPP_IMAGES/ENG/TRAIN_BATCH/Strawberry"
+    find_and_delete_duplicates(directory_path)
