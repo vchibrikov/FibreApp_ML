86e4ddb0ae50beacbf0a846eb8768e2ff177a194
Vadym Chibrikov <98614057+vchibrikov@users.noreply.github.com>
2024-10-22 14:59:41 +0200

    5_IMAGE_FILTER.py

--- 5_IMAGE_FILTER.py
+++ 5_IMAGE_FILTER.py
@@ -1,15 +1,12 @@
 import os
 import tensorflow as tf
 
-# Define paths to directories
-train_dir = '/Users/vadymchibrikov/Desktop/FIBREAPP_IMAGES/ENG/TRAIN_BATCH'
-test_dir = '/Users/vadymchibrikov/Desktop/FIBREAPP_IMAGES/ENG/TEST_BATCH'
+train_dir = '/Users/path/to/train/batch'
+test_dir = '/Users/path/to/test/batch'
 
-# Parameters
 img_height = 150
 img_width = 150
 
-# Function to check if a file is a valid image
 def is_image_file(filepath):
     try:
         img = tf.io.read_file(filepath)
@@ -18,7 +15,6 @@ def is_image_file(filepath):
     except (tf.errors.InvalidArgumentError, ValueError):
         return False
 
-# Function to remove invalid images
 def remove_invalid_images(directory):
     for subdir, _, files in os.walk(directory):
         for file in files:
@@ -27,7 +23,6 @@ def remove_invalid_images(directory):
                 print(f"Removing invalid file: {filepath}")
                 os.remove(filepath)
 
-# Remove invalid images from both directories
 remove_invalid_images(train_dir)
 remove_invalid_images(test_dir)
 
