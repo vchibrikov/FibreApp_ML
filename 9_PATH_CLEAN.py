4c80388c957eb9b9bbce4235b7180cc3b2f5d737
Vadym Chibrikov <98614057+vchibrikov@users.noreply.github.com>
2024-10-23 08:58:29 +0200

    9_PATH_CLEAN.py

--- 9_PATH_CLEAN.py
+++ 9_PATH_CLEAN.py
@@ -1,19 +1,14 @@
 import os
 
 def delete_files_in_directory(directory_path):
-    # Walk through the directory tree
     for root, dirs, files in os.walk(directory_path):
-        # Loop through each file in the current directory
         for file_name in files:
-            # Construct the full path to the file
             file_path = os.path.join(root, file_name)
             try:
-                # Delete the file
                 os.remove(file_path)
                 print(f"Deleted: {file_path}")
             except Exception as e:
                 print(f"Error deleting {file_path}: {e}")
 
-# Replace 'your_directory_path' with the path of your directory
-directory_path = '/Users/vadymchibrikov/Desktop/FIBREAPP_IMAGES/ENG_Android_clean'
+directory_path = '/Users/path/to/directory'
 delete_files_in_directory(directory_path)
